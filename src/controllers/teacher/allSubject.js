faf512a435ea78dc4dbf5423de5091ea5acff8dc
Rishav Mukherjee <rishavmukherjee11@gmail.com>
2023-10-12 19:17:18 +0530

    okkkkkkk

--- src/controllers/teacher/allSubject.js
+++ src/controllers/teacher/allSubject.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 
 const showAllIds = async (req, res) => {
   try {
-    const { stream, sem } = req.body;
+    const { stream } = req.body;
 
     const subjectModel = mongoose.model(stream, Subject.schema);
 
-    const subjectsMatchingSem = await subjectModel.find({ sem }, '_id');
+    const subjects = await subjectModel.find({}, '_id'); // Removed the 'sem' filter
 
-    const ids = subjectsMatchingSem.map(subject => subject._id);
+    const ids = subjects.map(subject => subject._id);
 
     res.status(200).json(ids);
   } catch (error) {
