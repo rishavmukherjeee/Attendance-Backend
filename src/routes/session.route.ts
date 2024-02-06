9cad8bfa91062c0604924aa0e4718a02db78118d
maaz619 <maazahmedlfc@gmail.com>
2024-02-06 19:22:59 +0530

    feat: get session by department

--- src/routes/session.route.ts
+++ src/routes/session.route.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { createSession, getAllSession, getSessionByDepartment, getSessionById } from "../controllers/session.controller"
+import { isAdmin, protect } from "../middlewares/auth.middleware";
+
 const router = Router()
 
+router.use([protect, isAdmin])
 router.post("/createOne", createSession)
 router.get("/", getAllSession)
 router.get("/:id", getSessionById)
